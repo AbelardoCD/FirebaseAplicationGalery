234dd3be175e2e01738eab24ddac38f0a7cf258d
AbelardoCD <57379915+AbelardoCD@users.noreply.github.com>
2020-09-06 12:23:13 -0500

    comit cambio img de fondo modulo descripcion

--- app/src/main/java/com/example/aplicationfirebase/MenuActivity.kt
+++ app/src/main/java/com/example/aplicationfirebase/MenuActivity.kt
@@ -3,6 +3,9 @@ package com.example.aplicationfirebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import com.example.aplicationfirebase.models.userPreferenceManager
 import kotlinx.android.synthetic.main.menu.*
@@ -13,25 +16,33 @@ class MenuActivity : AppCompatActivity() {
     lateinit var userPreference: userPreferenceManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.menu)
+
+        /* window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                         WindowManager.LayoutParams.FLAG_FULLSCREEN)
+                           */
+
+         setContentView(R.layout.menu)
+
+
+
         btnGaleria = findViewById(R.id.btnIrGaleria)
         btnNuevo = findViewById(R.id.btnIrVentanaNuevaObra)
 
         userPreference = userPreferenceManager(this)
         btnGaleria.setOnClickListener {
-            val intent =Intent(this,Home::class.java)
+            val intent = Intent(this, Home::class.java)
             startActivity(intent)
         }
 
 
         btnNuevo.setOnClickListener {
-            val intent =Intent(this,form_cargar_nueve_obra::class.java)
+            val intent = Intent(this, form_cargar_nueve_obra::class.java)
             startActivity(intent)
         }
 
         btncerrarSesion.setOnClickListener {
-          val cerrasrSesion =   userPreference.deleteUser()
-            val intent =Intent(this,login::class.java)
+            val cerrasrSesion = userPreference.deleteUser()
+            val intent = Intent(this, login::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/aplicationfirebase/obra_descripcion.kt
+++ app/src/main/java/com/example/aplicationfirebase/obra_descripcion.kt
@@ -1,15 +1,18 @@
 package com.example.aplicationfirebase
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toast
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.aplicationfirebase.models.Obra
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import jp.wasabeef.blurry.Blurry
 import kotlinx.android.synthetic.main.activity_obra_descripcion.*
+
 
 class obra_descripcion : AppCompatActivity() {
 
@@ -18,6 +21,9 @@ class obra_descripcion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_obra_descripcion)
+
+
+
 
         coneccion = coneccionFirebase(this)
         referencia = coneccion.getReferenciaObras()
@@ -43,17 +49,15 @@ class obra_descripcion : AppCompatActivity() {
         txtTituloObraDescripcion.text = nombre
         txtDescripcionDescripcion.text = descripcionObra
         txtAutorObraDescripcion.text = autor
-        Glide.with(this).load(url).into(imgObraDescripcion)
+
+        Glide.with(this).load(url).centerCrop().into(imgDescripcionFondo)
 
 
-       /* Blurry.with(this)
-            .radius(25)
-            .sampling(1)
-            .color(Color.argb(66, 0, 255, 255))
-            .async()
-            .capture(findViewById(R.id.imgObraDescripcion))
-            .into(findViewById(R.id.imgObraDescripcion))
-    */
+       // Blurry.with(this).capture(vistaDesenfocada).into(imgDescripcionFondo)
+
+        Glide.with(this).load(url).thumbnail().into(imgObraDescripcion)
+
+
     }
 
     private fun obtenemosId():String{
@@ -63,3 +67,6 @@ class obra_descripcion : AppCompatActivity() {
     }
 
 }
+
+
+
